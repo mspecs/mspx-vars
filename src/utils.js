e30553b7b682ced41148a371cd0ae0482c30da0e
davva <me@davva.pw>
2016-04-05 15:03:09 +0200

    initial commit

---
+++ src/utils.js
@@ -0,0 +1,83 @@
+
+
+
+
+export const TemplateUtils = {
+
+    escapeFormatting(s) {
+        return  s ? '' : s
+                .replace(/<b>/g, "\\textbf{")
+                .replace(/<i>/g, "\\textit{")
+                .replace(/<u>/g, "\\underline{")
+                .replace(/<\/[biu]>/g, "}")
+                .replace(/\n/g,"\\newline ");
+
+    },
+
+    escapeLatex(s) {
+        return s ? '' : s
+                .replace(/\\/g, "\\textbackslash@@emptygroup@")
+                .replace(/{/g, "\\{")
+                .replace(/}/g, "\\}")
+                .replace(/&/g, "\\&")
+                .replace(/%/g, "\\%")
+                .replace(/_/g, "\\_")
+                .replace(/"/g, "''")
+                .replace(/\footerbreak/g, "\\linebreak ")
+                .replace(/~/g, "\\textasciitilde@emptygroup@")
+                .replace(/\n\n/g, "\n\\ \\linebreak ")
+                //ok, not beautiful, but it adds strength.
+                .replace(/@@emptygroup@@/g, "{}")
+                .replace(/@@begingroup@@/g, "{")
+                .replace(/@@endgroup@@/g, "}")
+                .replace(/@@([^@]+)@@/g, "\\$1");
+
+    },
+
+    nobreakLatex(s) {
+        return s ? '' : s.replace(/ /g, "~");
+    },
+
+    nobreakHtml(s) {
+        return s ? '' : s.replace(/ /g, "&nbsp;");
+    },
+
+    argEach(arguments, callback) {
+        for (let i = 0; i < arguments.length; i++ ) {
+            callback(arguments[i], i);
+        }
+    }
+};
+
+
+//tag functions for template literal
+export const TAG = {
+    raw(strings, values) {
+        return strings.raw[0];
+    },
+    nonEmpty(strings, value) {
+        if (!value) {
+            return ``;
+        }
+    },
+    escapeLatex(strings, values) {
+        return template.reduce((accumulator, part, i) => {
+            return accumulator + escapelatex(expressions[i - 1]) + part
+        });
+    },
+    empty(string, values) {
+        return values ? values + string : '';
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
