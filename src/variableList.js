e4589e22f3bc65cb3e8b320b55d890dffc7b59fb
davva <me@davva.pw>
2016-05-17 10:40:15 +0200

    ADDED: bower stuff

--- src/variableList.js
+++ src/variableList.js
@@ -1,25 +1,24 @@
 
 
-
+/*
+* Variable meta data.
+* templateClasses lists all valid template classes and should be compatible with the "templateModels/index.js" naming scheme
+*
+*/
 module.exports = [
     {
         name : 'deal.broker',
         path :'deals.mainBrokerContactId',
-        templateClass:'contacts'
+        templateClasses:['Contact']
     },
-
     {
         name : 'deal.sellers',
-        path : 'deals.collections.sellers.contacts'
+        path : 'deals.collections.sellers.contacts',
+        templateClasses:['Contact']
     },
-
-    {
-        name : 'deal.seller.account',
-        path :''
-    },
-
     {
         name : 'deal.collections.buyers',
         path :'deals.buyerGroupId.collections.buyers.contacts',
+        templateClasses:['Contact']
     }
 ];
