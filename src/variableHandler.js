744799b67cabe75990e42cd805351b577139c486
davva <me@davva.pw>
2016-05-17 14:14:23 +0200

    REMOVED: TODO

--- src/variableHandler.js
+++ src/variableHandler.js
@@ -2,13 +2,6 @@
 var templates = require('./templateModels');
 var variables = require('./variableList.js');
 
-
-
-/*
-* TODO:window/global should only be used for frontend
-*
-*/
-
 var variableHandler = {
     //might not need to pass templateName to function, might want to just send all possible templates
     getVariable(variableName, templateName) {
